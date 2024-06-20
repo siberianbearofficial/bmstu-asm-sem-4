5935d5999d71783f4ea9462aa655d3d277e1424e
Aleksei Orlov <alexeyorlov65@gmail.com>
2024-06-20 20:54:55 +0300

    lab_07 strlen

--- lab_07/main.c
+++ lab_07/main.c
@@ -4,29 +4,32 @@
 // Объявление ассемблерной функции
 extern long add_numbers(long a, long b);
 
-long add_numbers_ins(long a, long b) {
-    long result;
+long my_strlen(char *str) {
+    long len;
 
-    // Ассемблерная вставка для 64-битных регистров
     __asm__ (
-            "addq %%rbx, %%rax;"
-            : "=a" (result)
-            : "a" (a), "b" (b)
-            );
+        "movq %%rbx, %%rdi;"    // кладем указатель на строку из bx в di
 
-    return result;
+        "xor %%rcx, %%rcx;"
+        "not %%rcx;"
+        "xor %%rax, %%rax;"     // будем сравнивать с нулем
+        "repne scasb;"          // ищем ax в строке по адресу di
+
+        "movq %%rdi, %%rax;"    // кладем конечное значение di в rax
+        "subq %%rbx, %%rax;"    // вычтем начальное значение указателя строки
+        "dec %%rax;"            // вычтем еще 1, так как di указывает на байт после нулевого символа
+
+        : "=a" (len)            // выходное значение (длина строки из ax)
+        : "b" (str)             // входное значение (указатель на строку в bx)
+    );
+
+    return len;
 }
 
 int main(void) {
-    long num1, num2, result;
-
-    scanf("%ld %ld", &num1, &num2);
-
-    result = add_numbers(num1, num2);
-    printf("Result: %ld\n", result);
-
-    result = add_numbers_ins(num1, num2);
-    printf("Result ins: %ld\n", result);
+    char s[] = "Hi bro I'm here";
+    long len = my_strlen(s);
+    printf("Strlen: %ld\n", len);
 
     return EXIT_SUCCESS;
 }
