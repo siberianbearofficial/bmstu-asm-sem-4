d045079176ecd01bab759568cde1cf2888e298b7
Aleksei Orlov <alexeyorlov65@gmail.com>
2024-06-25 17:23:42 +0300

    lab_10 init

---
+++ lab_10/root.h
@@ -0,0 +1,13 @@
+#ifndef ROOT_H
+#define ROOT_H
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define EPS 1e-6
+
+void str_func_root(char *start, char *end, char *step_count);
+
+#endif //ROOT_H
